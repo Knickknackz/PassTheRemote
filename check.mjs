cbbcfc6eeec19ec8f572c6d6df598d929fda0daf
John Carrigan <jcarrigan414@gmail.com>
2025-06-21 20:50:00 -0500

    More Deleted Files

--- check.mjs
+++
@@ -1,6 +0,0 @@
-import path from 'path';
-import fs from 'fs';
-
-const file = path.resolve('./src/crunchyrollParentPage.js');
-console.log('Exists:', fs.existsSync(file));
-console.log('Resolved path:', file);
